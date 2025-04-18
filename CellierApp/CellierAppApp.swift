f837098700211cfa61a115f60d427d287e5d62b9
VraiSlime <mathias.bouabdallah66@gmail.com>
2025-04-18 09:28:06 +0200

    Initial Commit

---
+++ CellierApp/CellierAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CellierAppApp.swift
+//  CellierApp
+//
+//  Created by Stagiaire on 18/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct CellierAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
